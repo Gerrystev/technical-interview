d985ba3159bb902073086c980a628b8a59b17e6f
Gerry Steven <sgerrys93@gmail.com>
2022-07-12 15:12:34 +0700

    Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register', 'refresh', 'logout']]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'username'    => 'required|string',
+                'password' => 'required|string|',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [$validator->errors()],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $user = User::create(
+            array_merge(
+                $validator->validated(),
+                ['password' => bcrypt($request->password)]
+            )
+        );
+
+        return response()->json(['message' => 'Success']);
+
+    }
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'username'    => 'required|string',
+                'password' => 'required|string',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        $token_validity = (24 * 60);
+
+        $this->guard()->factory()->setTTL($token_validity);
+
+        if (!$token = $this->guard()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->respondWithToken($token);
+
+    }
+
+
+    public function logout()
+    {
+        $this->guard()->logout();
+
+        return response()->json(['message' => 'Logged out!']);
+
+    }
+
+
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json(
+            [
+                'token'          => $token,
+                'token_type'     => 'bearer',
+                'token_validity' => ($this->guard()->factory()->getTTL() * 60),
+            ]
+        );
+
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 use Jenssegers\Mongodb\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model
+class User extends Model implements Authenticatable, JWTSubject
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, AuthenticatableTrait;
+
+    protected $fillable = [
+        'username',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
